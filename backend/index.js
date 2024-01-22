8d497828ac4277fd9bdfff903d547fca0b40f53a
Aurojyoti <developeratbackend@gmail.com>
2024-01-22 23:28:13 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const mainRouter = require("./routes/index");
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/v1", mainRouter);
+
+module.exports = router;
+
+app.listen(3000);
